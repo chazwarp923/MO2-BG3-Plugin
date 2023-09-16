527cfb9c48a318e077b5b950c8e3d3d0510e6bee
Chaz <chazwarp923@users.noreply.github.com>
2023-09-16 06:26:41 -0400

    Implement the basic logic to be able to start generating a modsettings.lsx

---
+++ baldursgate3/ModSettingsHelper.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+
+# import clr
+import mobase
+
+
+def generateSettings(pl: mobase.IPluginList) -> bool:
+    # clr.AddReference("LSLib.dll")
+
+    return True
--- game_baldursgate3.py
+++ game_baldursgate3.py
@@ -3,17 +3,19 @@
 import os
 from typing import List, Optional
 from PyQt5.QtCore import QDir, QFileInfo
-from pathlib import Path
 
 import mobase
 
 from ..basic_features import BasicGameSaveGameInfo
 from ..basic_game import BasicGame
 
+from .baldursgate3 import ModSettingsHelper
+
+
 class BaldursGate3Game(BasicGame, mobase.IPluginFileMapper):
     Name = "Baldur's Gate 3 Support Plugin"
     Author = "chazwarp923"
-    Version = "1.0.0"
+    Version = "2.0.0"
 
     GameName = "Baldur's Gate 3"
     GameShortName = "baldursgate3"
@@ -23,16 +25,16 @@ class BaldursGate3Game(BasicGame, mobase.IPluginFileMapper):
     GameSteamId = [1086940]
     GameGogId = [1456460669]
     GameBinary = "bin/bg3.exe"
-    #GameLauncher = "Launcher/LariLauncher.exe"
     GameDataPath = "Data"
-    GameSaveExtension = "lsv"  # Not confirmed
+    GameSaveExtension = "lsv"
     GameDocumentsDirectory = (
-        os.getenv('LOCALAPPDATA') + "/Larian Studios/Baldur's Gate 3"
+        os.getenv("LOCALAPPDATA") + "/Larian Studios/Baldur's Gate 3"
     )
     GameSavesDirectory = (
-        os.getenv('LOCALAPPDATA') + "/Larian Studios/Baldur's Gate 3/PlayerProfiles/Public/Savegames/Story"
+        os.getenv("LOCALAPPDATA")
+        + "/Larian Studios/Baldur's Gate 3/PlayerProfiles/Public/Savegames/Story"
     )
-    
+
     DOCS_MOD_SPECIAL_NAME = "PAK_FILES"
 
     def __init__(self):
@@ -41,17 +43,16 @@ class BaldursGate3Game(BasicGame, mobase.IPluginFileMapper):
 
     def init(self, organizer: mobase.IOrganizer):
         super().init(organizer)
-        
+
         self._featureMap[mobase.SaveGameInfo] = BasicGameSaveGameInfo(
             lambda s: s.replace(".lsv", ".webp")
         )
-        
-        self._featureMap[
-            mobase.ModDataChecker
-        ] = BaldursGate3ModDataChecker()
-        
-        
-        
+
+        self._featureMap[mobase.ModDataChecker] = BaldursGate3ModDataChecker()
+
+        # callback = ModSettingsHelper.generateSettings()
+        self._organizer.onAboutToRun(self.aboutToRunCallback)
+
         return True
 
     def executables(self):
@@ -63,10 +64,11 @@ class BaldursGate3Game(BasicGame, mobase.IPluginFileMapper):
                 "BG3 - DX11", QFileInfo(self.gameDirectory(), "bin/bg3_dx11.exe")
             ).withArgument("--skip-launcher"),
             mobase.ExecutableInfo(
-                "Larian Launcher", QFileInfo(self.gameDirectory(), "Launcher/LariLauncher.exe")
+                "Larian Launcher",
+                QFileInfo(self.gameDirectory(), "Launcher/LariLauncher.exe"),
             ),
         ]
-        
+
     def mappings(self) -> List[mobase.Mapping]:
         map = []
         modDirs = [self.DOCS_MOD_SPECIAL_NAME]
@@ -81,7 +83,6 @@ class BaldursGate3Game(BasicGame, mobase.IPluginFileMapper):
                     self.documentsDirectory().absoluteFilePath("Mods"),
                     file_.split(self.DOCS_MOD_SPECIAL_NAME)[1].strip("\\").strip("/"),
                 )
-                print(dir_)
                 map.append(m)
         return map
 
@@ -91,18 +92,24 @@ class BaldursGate3Game(BasicGame, mobase.IPluginFileMapper):
             dir_ = os.path.join(prefix, dir_)
             dirs_list.append(dir_)
             self._listDirsRecursive(dirs_list, dir_)
-            
-    def primarySources(self):
-        return self.GameValidShortNames
+
+    # def primarySources(
+    # self,
+    # ):  # Not sure this function does anything, but I don't know enough about python to dare remove it
+    # return self.GameValidShortNames
+
+    def aboutToRunCallback(self, path: str) -> bool:
+        ModSettingsHelper.generateSettings(self._organizer.pluginList())
+        return True
+
 
 class BaldursGate3ModDataChecker(mobase.ModDataChecker):
     def __init__(self):
         super().__init__()
 
     def dataLooksValid(
-            self, tree: mobase.IFileTree
+        self, tree: mobase.IFileTree
     ) -> mobase.ModDataChecker.CheckReturn:
-
         folders: List[mobase.IFileTree] = []
         files: List[mobase.FileTreeEntry] = []
 
