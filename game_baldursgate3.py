61557a90f89ad201c25328b6f8d4054593e36814
Chaz <chazwarp923@users.noreply.github.com>
2024-10-16 01:34:13 -0400

    Add Licenses

--- game_baldursgate3.py
+++ game_baldursgate3.py
@@ -14,7 +14,7 @@ from .baldursgate3 import ModSettingsHelper
 class BaldursGate3Game(BasicGame, mobase.IPluginFileMapper):
     Name = "Baldur's Gate 3 Unofficial Support Plugin"
     Author = "chazwarp923 & Dragozino"
-    Version = "2.2.0"
+    Version = "2.3.0"
 
     GameName = "Baldur's Gate 3"
     GameShortName = "baldursgate3"
